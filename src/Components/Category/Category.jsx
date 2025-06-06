d3a40cd3f2c368cc25e75c36a3c4183af2b933b1
adismule <adismule@gmail.com>
2025-06-06 10:41:07 -0700

    category.module.css updatetd

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { categoryInfo } from './CategoryFullInfo'
 import CategoryCard from './CategoryCard'
-import classes from './category.module.css'
+import classes from './Category.module.css'
 
 function Category() {
   return (
